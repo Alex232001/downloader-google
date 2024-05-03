dc3e2bb78f2abcdacf6d2f3c5c05e630dc369d0f
Alex232001 <161832267+Alex232001@users.noreply.github.com>
2024-05-03 16:21:51 +0700

    Update colab.bash

--- colab.bash
+++ colab.bash
@@ -3,10 +3,11 @@ pwd;
 read -p "URL: " URL;
 echo $URL;
 wget $URL;
-echo "Raw не обновляется";
 
 filename=$(echo "$URL" | grep -o '[^/]*$');
 echo "$filename";
 
 path_to_torrent=/content/$filename;
 echo $path_to_torrent;
+
+aria2c -x 10 -s 10 --allow-overwrite --disable-ipv6 --seed-time=0 --seed-ratio=0.0  $path_to_torrent;
